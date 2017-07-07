70deeb68b2cf8a04d00fad8260e28216c94b07b1
Lemonszz <lemonverse@gmail.com>
2017-07-07 15:27:17 +1200

    Fix ender shard furnace recipe not working

--- src/main/java/lemons/party/teleporters/proxy/CommonProxy.java
+++ src/main/java/lemons/party/teleporters/proxy/CommonProxy.java
@@ -1,24 +1,13 @@
 package lemons.party.teleporters.proxy;
 
 import lemons.party.teleporters.Constants;
-import lemons.party.teleporters.content.blocks.TeleportersBlocks;
 import lemons.party.teleporters.content.config.ModConfig;
 import lemons.party.teleporters.content.crafting.PositionDupeRecipe;
-import lemons.party.teleporters.content.event.TeleporterEvents;
 import lemons.party.teleporters.content.items.TeleportersItems;
 import lemons.party.teleporters.content.tileentity.TileEntityTeleporter;
-import net.minecraft.client.renderer.block.model.ModelBlock;
-import net.minecraft.client.util.RecipeItemHelper;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.IRecipe;
-import net.minecraft.item.crafting.Ingredient;
-import net.minecraft.item.crafting.ShapedRecipes;
-import net.minecraft.util.NonNullList;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -27,7 +16,6 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.oredict.ShapedOreRecipe;
 
 @Mod.EventBusSubscriber
 public class CommonProxy
@@ -37,8 +25,6 @@ public class CommonProxy
 		ModConfig.loadConfig(new Configuration(event.getSuggestedConfigurationFile()));
 
 		GameRegistry.registerTileEntity(TileEntityTeleporter.class, "lTele");
-
-		GameRegistry.addSmelting(Items.ENDER_EYE, new ItemStack(TeleportersItems.teleCrystal), 0.1f);
 	}
 
 	@SubscribeEvent
@@ -47,7 +33,9 @@ public class CommonProxy
 		event.getRegistry().register(new PositionDupeRecipe().setRegistryName(Constants.MODID, "posdupe"));
 	}
 
-	public void init(FMLInitializationEvent event) {
+	public void init(FMLInitializationEvent event) 
+	{
+		GameRegistry.addSmelting(Items.ENDER_EYE, new ItemStack(TeleportersItems.teleCrystal), 0.1f);
 	}
 
 	public void postInit(FMLPostInitializationEvent event) {
